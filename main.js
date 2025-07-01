84f8c823327a13dd8fda6c6f25ba71a377488d52
Awino Cissé <aawacisse@gmail.com>
2025-07-01 20:17:27 +0000

    update 7/1/25

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("javascript est connecter !!!");
+const scrollUpBtn = document.getElementById('scrollup');
+
+
+// Afficher le bouton seulement quand on descend un peu
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollUpBtn.style.display = 'block';
+  } else {
+    scrollUpBtn.style.display = 'none';
+  }
+});
+
+ scrollUpBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+ });
